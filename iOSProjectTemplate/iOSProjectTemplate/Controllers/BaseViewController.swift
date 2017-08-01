1153bd23ff1ba598cf484bd1626fef9681f24540
Khoi Nguyen <nguyenkhoinguyen082444@gmail.com>
2017-08-01 11:27:10 +0700

    Organize project folder

--- iOSProjectTemplate/iOSProjectTemplate/Controllers/BaseViewController.swift
+++ iOSProjectTemplate/iOSProjectTemplate/Controllers/BaseViewController.swift
@@ -14,6 +14,9 @@ class BaseViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        initUI()
+        initData()
+        registerNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +25,18 @@ class BaseViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // initialize UI when view didload
+    func initUI(){
+        
     }
-    */
+    
+    // initialize data when view didload
+    func initData(){
+        
+    }
 
+    // register notifications
+    func registerNotifications(){
+        
+    }
 }
